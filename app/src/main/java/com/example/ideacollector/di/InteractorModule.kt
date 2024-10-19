ce00076e78e7ea96e74167ad06b695276410a5f0
Natalia Kozhanova <natalia.v.kozhanova@gmail.com>
2024-10-19 19:37:25 +0200

    project preparation

---
+++ app/src/main/java/com/example/ideacollector/di/InteractorModule.kt
@@ -0,0 +1,7 @@
+package com.example.ideacollector.di
+
+import org.koin.dsl.module
+
+val interactorModule = module {
+
+}
---
+++ app/src/main/java/com/example/ideacollector/root/ui/RootActivity.kt
@@ -0,0 +1,10 @@
+package com.example.ideacollector.root.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class RootActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
